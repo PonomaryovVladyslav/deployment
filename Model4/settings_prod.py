412357d991400fee38ea02978ea52444a5178292
PonomaryovVladyslav <Blackbird1Blackbird1!>
2020-12-14 03:12:19 +0200

    debug

--- Model4/settings_prod.py
+++ Model4/settings_prod.py
@@ -1,8 +1,8 @@
 import os
 from .settings import INSTALLED_APPS
 
-DEBUG = False
-ALLOWED_HOSTS = ['34.221.249.152']
+# DEBUG = False
+# ALLOWED_HOSTS = ['34.221.249.152']
 
 DATABASES = {
     'default': {
